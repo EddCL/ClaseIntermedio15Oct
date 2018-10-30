6b4b7b4944a652cd02b1e03611f09aff3c6e5d84
EddCL <38968021+EddCL@users.noreply.github.com>
2018-10-29 18:07:07 -0600

    Add files via upload

---
+++ Semaforo/Semaforo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Semaforo
+//
+//  Created by MacBook on 29/10/18.
+//  Copyright © 2018 potato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func unwindtoRed(unwindSegue: UIStoryboardSegue){
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
