db34c6edf06b14926b84228aab8df50d7b487840
EddCL <38968021+EddCL@users.noreply.github.com>
2018-11-07 18:55:32 -0600

    Add files via upload

---
+++ PersonalityQuiz/PersonalityQuiz/QuestionViewController.swift
@@ -0,0 +1,37 @@
+//
+//  QuestionViewController.swift
+//  PersonalityQuiz
+//
+//  Created by MacBook on 07/11/18.
+//  Copyright © 2018 potato. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+//    var questions: [Question] = [Question(text: "Which food do you like the most?", type: .single. answer: [Answer(text:"Steak", type: .dog),])
+//    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
